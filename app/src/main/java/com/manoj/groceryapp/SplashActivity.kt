a471ee15b0b933e916a752df283b25f0359d512f
manojs2001 <manojs231@outlook.com>
2022-09-13 20:38:17 +0530

    final commit

---
+++ app/src/main/java/com/manoj/groceryapp/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.manoj.groceryapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.widget.ProgressBar
+import manoj.groceryapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+         val SPLASH_TIME_OUT = 3000
+         val TAG = "Splash_Screen"
+        var progressBar: ProgressBar? = null
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        progressBar = findViewById(R.id.ProgressBar)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_TIME_OUT.toLong())
+    }
+}
